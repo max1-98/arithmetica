8333c1fcb9d5756bc68c255d4abf4f3ef42d9fa6
max1-98 <maxsmithbusiness198@gmail.com>
2022-11-12 18:38:33 +0000

    Minor edits

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
 
-from core.models import Sample
+from core.models import Sample, StudentLessons, Students, Lesson, OneStudent
 
 admin.site.register(Sample)
+admin.site.register(StudentLessons)
+admin.site.register(Students)
+admin.site.register(Lesson)
+admin.site.register(OneStudent)
+
--- scripts/run.sh
+++ scripts/run.sh
@@ -4,11 +4,5 @@ set -e
 
 python manage.py wait_for_db
 python manage.py collectstatic --noinput
-python manage.py makemigrations core
-python manage.py migrate core
-python manage.py makemigrations register
-python manage.py migrate
-python manage.py makemigrations
-python manage.py migrate
 
 uwsgi --socket :9000 --workers 4 --master --enable-threads --module app.wsgi
